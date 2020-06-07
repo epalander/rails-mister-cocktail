16fec21c5cdf7e0ce47fc3cbd790440d38c68157
Elisabeth Palander <epalander@gmail.com>
2020-06-07 13:46:30 +0200

    set index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
